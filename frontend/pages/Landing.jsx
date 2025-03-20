1df12039b991089d781b00b7b74f9e1c544699c3
itzRohit45 <rohit9060755@gmail.com>
2025-03-20 14:13:03 +0530

    logout,profile and dashboard

--- frontend/pages/Landing.jsx
+++ frontend/pages/Landing.jsx
@@ -26,7 +26,7 @@ const Landing = () => {
         <About toggleDone={toggleDone} />
       ) : (
         <div className="landing-main">
-          <h1>QR Scan</h1>
+          <h1>ScanMe</h1>
           <p>Hello and welcome!</p>
           <Link to="/login" className="landing-login-button">
             Login
