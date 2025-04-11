1ab3ea032a16996e18fad20d5aac78f50bc86296
itzRohit45 <rohit9060755@gmail.com>
2025-04-11 20:33:43 +0530

    email,forgot password,signup change

--- frontend/pages/Index.jsx
+++ frontend/pages/Index.jsx
@@ -5,6 +5,5 @@ export { default as Login } from "./Login";
 export { default as Register } from "./Signup";
 export { default as Logout } from "./Logout";
 export { default as Nav } from "./Nav";
-export { default as NewSession } from "./NewSession";
 export { default as StudentDashboard } from "./StudentDashboard";
 export { default as ForgotPassword } from "./ForgotPassword";
